fab915f3a85c5f2096fce9f1c8c4f15c966cdd9f
Iago Cappeletti <Iago_cappeletti@hotmail.com>
2025-01-27 00:17:09 -0300

    feat: New registration flow

---
+++ src/main/java/br/com/alura/ProjetoAlura/registration/CourseRegistration.java
@@ -0,0 +1,68 @@
+package br.com.alura.ProjetoAlura.registration;
+
+import br.com.alura.ProjetoAlura.course.Course;
+import br.com.alura.ProjetoAlura.user.User;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "registration")
+public class CourseRegistration {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id", nullable = false)
+    private Course course;
+
+    @Column(name = "registration_date", nullable = false, updatable = false)
+    private LocalDateTime registrationDate = LocalDateTime.now();
+
+    @Deprecated
+    public CourseRegistration() {}
+
+    public CourseRegistration(User user, Course course, LocalDateTime registrationDate) {
+        this.user = user;
+        this.course = course;
+        this.registrationDate = registrationDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public LocalDateTime getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(LocalDateTime registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+}
---
+++ src/main/java/br/com/alura/ProjetoAlura/registration/RegistrationRepository.java
@@ -0,0 +1,28 @@
+package br.com.alura.ProjetoAlura.registration;
+
+import br.com.alura.ProjetoAlura.course.Course;
+import br.com.alura.ProjetoAlura.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface RegistrationRepository extends JpaRepository<CourseRegistration, Long> {
+
+    boolean existsByUserAndCourse(User user, Course course);
+
+    @Query("""
+    SELECT new br.com.alura.ProjetoAlura.registration.CourseRegistrationDetailsDTO(
+        c.name,
+        c.code,
+        c.instructorName,
+        c.instructorEmail,
+        COUNT(r.course.id)
+    )
+    FROM Course c
+    LEFT JOIN CourseRegistration r ON c.id = r.course.id
+    WHERE c.status = 'ACTIVE'
+    GROUP BY c.name, c.code, c.instructorName, c.instructorEmail
+""")
+    List<CourseRegistrationDetailsDTO> findCourseDetailsWithRegistrationCount();
+}
