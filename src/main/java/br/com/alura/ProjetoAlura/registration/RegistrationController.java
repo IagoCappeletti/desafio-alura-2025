1c8c8804607d0df90b44bfbb3f522c18b327f6e4
Iago Cappeletti <Iago_cappeletti@hotmail.com>
2025-01-27 00:08:58 -0300

    feat: New registration controller

--- src/main/java/br/com/alura/ProjetoAlura/registration/RegistrationController.java
+++ src/main/java/br/com/alura/ProjetoAlura/registration/RegistrationController.java
@@ -1,56 +1,78 @@
 package br.com.alura.ProjetoAlura.registration;
 
+import br.com.alura.ProjetoAlura.course.Course;
+import br.com.alura.ProjetoAlura.course.CourseRepository;
+import br.com.alura.ProjetoAlura.course.CourseStatus;
+import br.com.alura.ProjetoAlura.user.User;
+import br.com.alura.ProjetoAlura.user.UserRepository;
+import br.com.alura.ProjetoAlura.util.ErrorItemDTO;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 public class RegistrationController {
 
-    @PostMapping("/registration/new")
-    public ResponseEntity createCourse(@Valid @RequestBody NewRegistrationDTO newRegistration) {
-        // TODO: Implementar a Questão 3 - Criação de Matrículas aqui...
+    @Autowired
+    CourseRepository courseRepository;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    RegistrationRepository registrationRepository;
 
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+    @PostMapping("/registration/new")
+    public ResponseEntity createCourse(@Valid @RequestBody NewRegistrationDTO newRegistration, UriComponentsBuilder uriBuilder) {
+        Optional<Course> course = courseRepository.findByCode((newRegistration.getCourseCode()));
+        if (course.isEmpty() || course.get().getStatus() != CourseStatus.ACTIVE) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(new ErrorItemDTO("Code", "Course does not exist or is inactive! Please try again with another code."));
+        }
+
+        Optional<User> user = userRepository.findByEmail(newRegistration.getStudentEmail());
+        if (user.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(new ErrorItemDTO("Email", "Non-existent user!"));
+        }
+
+        if (registrationRepository.existsByUserAndCourse(user.get(), course.get())) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(new ErrorItemDTO("Code", "User is already registered in this course"));
+        }
+
+        CourseRegistration registration = new CourseRegistration();
+        registration.setCourse(course.get());
+        registration.setUser(user.get());
+        registrationRepository.save(registration);
+
+        var uri = uriBuilder.path("/registration/{id}").buildAndExpand(registration.getId()).toUri();
+        return ResponseEntity.created(uri).body(registration);
+
+
     }
 
     @GetMapping("/registration/report")
     public ResponseEntity<List<RegistrationReportItem>> report() {
-        List<RegistrationReportItem> items = new ArrayList<>();
 
-        // TODO: Implementar a Questão 4 - Relatório de Cursos Mais Acessados aqui...
-
-        // Dados fictícios abaixo que devem ser substituídos
-        items.add(new RegistrationReportItem(
-                "Java para Iniciantes",
-                "java",
-                "Charles",
-                "charles@alura.com.br",
-                10L
-        ));
-
-        items.add(new RegistrationReportItem(
-                "Spring para Iniciantes",
-                "spring",
-                "Charles",
-                "charles@alura.com.br",
-                9L
-        ));
-
-        items.add(new RegistrationReportItem(
-                "Maven para Avançados",
-                "maven",
-                "Charles",
-                "charles@alura.com.br",
-                9L
-        ));
+        var courseRanked =  registrationRepository.findCourseDetailsWithRegistrationCount();
+        List<RegistrationReportItem> items = courseRanked.stream()
+                .map(courseDetail -> new RegistrationReportItem(
+                        courseDetail.name(),
+                        courseDetail.code(),
+                        courseDetail.instructorName(),
+                        courseDetail.instructorEmail(),
+                        courseDetail.registrationCount()
+                ))
+                .collect(Collectors.toList());
 
         return ResponseEntity.ok(items);
     }
